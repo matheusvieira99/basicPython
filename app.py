489dce03557078d945be00b5c3b8aad9a2b2b8a5
Matheus Vieira <matheus-vieira12@hotmail.com>
2021-08-15 22:15:02 -0300

    getting started

---
+++ app.py
@@ -0,0 +1,8 @@
+name = "Matheus"
+age = 21
+is_male = True
+print("I'm called " + name + " and I'm a male", is_male)
+print("I'm", age, "years old")
+name = "John"
+print("I really like to be named " + name)
+print("But I actually don't like being", age, "years old")
