c770ac7d9155f96969d498aa161219007c6a33e9
Matheus Vieira <matheus-vieira12@hotmail.com>
2021-08-15 22:31:00 -0300

    add: condition and inputs

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 name = "Matheus"
 age = 21
 is_male = True
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+is_male = input("Are you a male? (y/n)")
+if (is_male == "y" or is_male == "Y"):
+    is_male = True
+else:
+    is_male = False
 print("I'm called " + name + " and I'm a male", is_male)
 print("I'm", age, "years old")
-name = "John"
 print("I really like to be named " + name)
 print("But I actually don't like being", age, "years old")
